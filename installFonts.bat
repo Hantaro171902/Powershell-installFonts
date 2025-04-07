1b306ee1f57d992a64f5eb2b65ba70bde54a1c53
Chau Han <chaugiahan001@gmail.com>
2025-04-07 14:07:54 +0700

    update .bat file

---
+++ installFonts.bat
@@ -0,0 +1,30 @@
+@echo off
+setlocal enabledelayedexpansion
+
+:: Set your fonts source folder here
+:: Thêm đường dân đến thư mục chứa font vào biến fontFolder
+set "fontFolder=X:\Thư mục của bạn\..."
+
+:: System fonts directory
+set "fontsDir=%WINDIR%\Fonts"
+
+:: Function to install fonts
+echo Installing .otf fonts...
+for /r "%fontFolder%" %%F in (*.otf) do (
+    set "fontFile=%%~nxF"
+    echo Installing: !fontFile!
+    copy "%%F" "%fontsDir%" >nul
+    reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Fonts" /v "!fontFile!" /t REG_SZ /d "!fontFile!" /f >nul
+)
+
+echo Installing .ttf fonts...
+for /r "%fontFolder%" %%F in (*.ttf) do (
+    set "fontFile=%%~nxF"
+    echo Installing: !fontFile!
+    copy "%%F" "%fontsDir%" >nul
+    reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Fonts" /v "!fontFile!" /t REG_SZ /d "!fontFile!" /f >nul
+)
+
+echo.
+echo All fonts installed successfully!
+pause
